c2cb1332ded40c189b6398131562a0de7003c9c3
longmen2019 <51213632+longmen2019@users.noreply.github.com>
2021-03-26 23:41:46 -0400

    Add files via upload

---
+++ Dunder or magic methods in Python.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 26 23:22:17 2021
+
+@author: User
+"""
+
+# #declare our own string class 
+# class Word:
+# #magic method to initiate object:
+#     def __init__(self, str):
+#         self.str = str
+# #driver code:
+# if __name__ == "__main__":
+# #object creation:
+#     # string1 = Word ("Hello")
+#     string1 = Word ('Hello')
+# #print object location
+#     print(string1)
+    
+#declare our own string class
+# class String():
+#     #magic method to initiate object:
+#     def __init__(self , str):
+#         self.str = str
+#     #print out string object
+#     def __repr__(self):
+#         return "object : {}" .format(self.str)
+#     #driver code
+# if __name__ == "__main__":
+#     #object creation
+#     string1 = String("Hello")
+#     print(string1)
+
